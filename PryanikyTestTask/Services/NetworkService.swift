f7c8aa8a3fb48a7f0119ead00761b5a3bde9de2d
Evgeny Novgorodov <greatly01@ya.ru>
2021-03-26 00:52:40 +0500

    NetworkService was updated to work with Combine

--- PryanikyTestTask/Services/NetworkService.swift
+++ PryanikyTestTask/Services/NetworkService.swift
@@ -6,16 +6,32 @@
 //
 
 import Alamofire
+import Combine
 
 protocol NetworkServiceProtocol {
     func fetchViewData(completion: @escaping (Result<DataModel, Error>) -> Void)
+    func fetchDataWithCombine(completion: @escaping (Result<DataModel, Error>) -> Void)
 }
     
 final class NetworkService: NetworkServiceProtocol {
     
+    // MARK: - Static properties
+    
+    static let shared = NetworkService.init()
+    
+    // MARK: - Properties
+    
 //    private let url = "https://pryaniky.com/static/json/sample.json"
 //    private let url = "https://chat.pryaniky.com/json/data-custom-selected-id.json"
-    private let url = "https://chat.pryaniky.com/json/data-custom-order-much-more-items-in-data.json"
+    private let url = URL(string: "https://chat.pryaniky.com/json/data-custom-order-much-more-items-in-data.json")!
+    
+    private var subsciptions = Set<AnyCancellable>()
+    
+    // MARK: - Initializers
+    
+    private init() {}
+    
+    // MARK: - Public methods
     
     func fetchViewData(completion: @escaping (Result<DataModel, Error>) -> Void) {
         AF.request(url).responseDecodable(of: DataModel.self) { response in
@@ -27,4 +43,25 @@ final class NetworkService: NetworkServiceProtocol {
             }
         }
     }
+    
+    func fetchDataWithCombine(completion: @escaping (Result<DataModel, Error>) -> Void) {
+        URLSession.shared
+            .dataTaskPublisher(for: url)
+            .tryMap { data, response in
+                guard let httpResponse = response as? HTTPURLResponse,
+                      httpResponse.statusCode == 200 else {
+                    throw URLError(.badServerResponse)
+                }
+                return data
+            }
+            .decode(type: DataModel.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: {
+                print ("Received completion: \($0).")
+            }, receiveValue: { dataModel in
+                completion(.success(dataModel))
+            })
+            .store(in: &subsciptions)
+    }
 }
+
+
